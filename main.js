00fea9a6b06b25df20f31b9b399d8ff66d5ff27c
Neboer <43609792+Neboer@users.noreply.github.com>
2019-01-16 12:55:12 +0800

    消息传递逻辑，有bug等待修复

--- main.js
+++ main.js
@@ -1,21 +1,72 @@
-import Vue from "static/vue.min";
-const sub = document.getElementById("submit");
-const username = document.getElementById("username");
-const mes = document.getElementById("message");
-const request = new XMLHttpRequest();
-sub.onclick = function () {
-    let url = "send";
-    request.open("POST", url, true);
-    request.setRequestHeader("authorization", username.value);
-    request.send(mes.value);
+import Vue from 'static/vue.min';
+
+const submitButton = document.getElementById('submit');
+const username = document.getElementById('username');
+const mes = document.getElementById('message');
+const loginButton = document.getElementById('login');
+let newWebSocket = null;
+
+// const request = new XMLHttpRequest();
+function addMessageToDocument(messageSender, messageContent) {
+    thisVue.$data.sites.push({text: username.value + ':' + mes.value});
+}
+
+//服务器与客户端的模拟握手的原理：客户端首先向服务器发起连接请求，连接打开后，客户端立即响应发送用户名,服务器处理用户名，连接正式建立。
+//客户端发送消息，仅仅是发送消息，并不做任何动作。
+//服务器接受到一条客户端发送的消息后，将这条消息存储，然后尝试广播给包含发送者在内的所有用户。
+loginButton.onclick = function() {
+    newWebSocket = new WebSocket('ws://localhost:8081');//发起连接请求
+    newWebSocket.onopen = function(ev) {//连接打开且有效，客户端准备发送用户名
+        for(let i =0;i<=100;i++){
+            newWebSocket.send(encodeURI(username.value));//客户端发送用户名
+        }
+        newWebSocket.onmessage = function(messageEvent2) {//信息监听
+            let s = JSON.parse(messageEvent2.data);//信息内容（JSON串）
+            addMessageToDocument(s.poster, s.messageContent);//将信息打印在屏幕上
+        };
+    };
+    loginButton.hidden = Boolean(1);//隐藏登录按钮，连接正式建立。
 };
 
-// Vue.component('messager',{
-//     template:"<h1>自定义组件!</h1>"
-// });
-new Vue({
-    el: '#mess',
+// const messageList = {
+//     numberOfMessage: 0,
+//     poster: [],
+//     messageContent: [],
+//     addPost: function (posterName, postMessage) {
+//         this.poster.push(posterName);
+//         this.messageContent.push(postMessage);
+//         this.numberOfMessage++
+//     },
+//     returnPostList: function (callback) {
+//         let cachePosterList = [];
+//         for (let i = 0; i <= this.numberOfMessage - 1; i++) {
+//             cachePosterList.push(callback(this.poster[i],this.messageContent[i]))
+//         }
+//         return cachePosterList
+//     }
+// };
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<dd>{{ todo.text }}</dd>'
+});
+
+let thisVue = new Vue({
+    el: '#app',
     data: {
-        mess: "ojg!"
+        sites: []
     }
 });
+submitButton.onclick = function() {
+    //send the message to server
+    let usernameWaitForSend = encodeURI(username.value);
+    let messageContentWaitForSend = encodeURI(mes.value);
+    let messageWaitForSend = {poster: usernameWaitForSend, messageContent: messageContentWaitForSend};
+    newWebSocket.send(JSON.stringify(messageWaitForSend));//仅仅是发送信息而已，并不把信息增加到页面上，等待服务端回传信息时添加。
+    // let url = "send";
+    // request.open("POST", url, true);
+    // request.setRequestHeader("authorization", encodeURI(username.value));
+    // request.send(mes.value);
+    // addMessageToDocument(username.value, mes.value);
+};
+
--- server.js
+++ server.js
@@ -1,47 +1,111 @@
-const existFile = ["/chat.html", "/404.html", "/main.js", "/server.js", "/favicon.ico", "/mainsolve.js","/static/vue.min.js"];// First is main page,else are file exist on the server.
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-const url = require("url");
-// const express = require("express");
-// const bodyPraser = require("body-parser");
-// let app = express();
-http.createServer(function (request, response) {
-    console.log(request.url);
+const existFile = ['/chat.html', '/404.html', '/main.js', '/server.js', '/favicon.ico', '/mainsolve.js', '/static/vue.min.js'];// First is main page,else are file exist on the server.
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const mysql = require('mysql');
+const ws = require('ws').Server;
+// main http server
+const httpServer = http.createServer(function(request, response) {
+    // console.log(request.url);
     let requestLink = url.parse(request.url).path;
-    if (requestLink.localeCompare("/") === 0) {// main page logic
+    if (requestLink.localeCompare('/') === 0) { // main page logic
         requestLink = existFile[0];
     }
-    if (existFile.includes(requestLink)) {//return the file
+    if (existFile.includes(requestLink)) { //return the file
         let realPath = path.join(__dirname, requestLink);
-        fs.readFile(realPath, function (err, data) {
-            if (err) console.log(err.stack);
-            else {
+        fs.readFile(realPath, function(err, data) {
+            if (err) {
+                console.log(err.stack);
+            } else {
                 response.writeHead(200);
-                response.write(data, "binary");
+                response.write(data, 'binary');
                 response.end();
             }
-        })
-    } else if (requestLink === "/send" && request.method === "POST") {
-        let buffer = "";
-        let username = request.headers.authorization;
-        request.on("data", function (chunk) {
-            buffer += chunk;
         });
-        request.on("end", function () {
-            console.log(username + ":" + buffer);// will be replaced with ...
-            buffer = "";
-        })
-    } else {
+    }
+    // receive POST data
+    // else if (requestLink === '/send' && request.method === 'POST') {
+    //     let buffer = '';
+    //     let username = decodeURI(request.headers.authorization);
+    //     request.on('data', function(chunk) {
+    //         buffer += chunk;
+    //     });
+    //     request.on('end', function() {
+    //         console.log(username + ':' + buffer);// will be replaced with ...
+    //         buffer = '';
+    //     });
+    // }
+    else {
         let realPath = path.join(__dirname, existFile[1]);
-        fs.readFile(realPath, function (err, data) {
-            if (err) console.log(err.stack);
-            else {
+        fs.readFile(realPath, function(err, data) {
+            if (err) {
+                console.log(err.stack);
+            } else {
                 response.writeHead(404);
-                response.write(data, "binary");
+                response.write(data, 'binary');
                 response.end();
             }
-        })
+        });
     }
 }).listen(8080);
-console.log("http://localhost:8080/");
+console.log('http://localhost:8080/');
+
+// mysql service
+// const connection = mysql.createConnection({
+//     'host': 'localhost'
+//
+// });
+
+// const onlineList = {
+//     usernameList: [],
+//     socketList: [],
+//     totalClient: 0,
+//     addList: function(username, socket) {
+//         this.usernameList.push(username);
+//         this.socketList.push(socket);
+//         this.totalClient++;
+//     }
+// };
+let client = {};
+// function messagePiece(poster,messageContent,postDate){
+//     this.poster=poster;
+//     this.messageContent = messageContent;
+//     this.postDate = postDate;
+//     this.toJSON = function() {
+//         let a = {};
+//         a.poster = encodeURI(this.poster);
+//         a.messageContent = encodeURI(this.messageContent);
+//         a.postDate = this.postDate;
+//         return a;
+//     }
+// }
+
+//websocket service
+webSocketServer = new ws({
+    server: httpServer,
+    port: 8081
+});
+webSocketServer.on('connection', function(webSocket) {//服务器建立监听，等待客户端连接
+    console.log("connect");
+    webSocketServer.on('message', function(encodeUsername) {//一旦连接建立，服务器就等待客户端发送用户名
+        let username = decodeURI(encodeUsername);
+        console.log(username);
+        if(client[username]==null) {client[username] = webSocket}//服务器收到用户名，将其和socket一起加入等待列表
+        //服务器尝试从数据库中获取陈年老消息传递给连接上的用户
+        webSocketServer.on("message",function(newMessage) {//服务器修改监听状态，收到新消息立即广播给所有socket
+            console.log(newMessage);
+            //服务器尝试记录这条数据
+            for(let username in client){
+                if(client.hasOwnProperty(username)) client[username].send(newMessage);//服务器广播json串数据。
+            }
+        })
+    });
+});
+// webSocketServer.on('message', function(messageSend) {//messageSend is a JSON
+//     // write message in database
+//     // distribute the message to every client
+//     for (let i = 0; i <= onlineList.totalClient - 1; i++) {
+//         onlineList.socketList[i].send(JSON.parse(messageSend).messageContent)
+//     }
+// });
