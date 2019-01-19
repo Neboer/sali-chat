624a8246869b0f8ed42db85db070d6c6b699af3a
Neboer <43609792+Neboer@users.noreply.github.com>
2019-01-19 13:16:32 +0800

    Merge branch 'master' of E:\my Web project\sali-chat(bak) with conflicts.

--- main.js
+++ main.js
@@ -5,45 +5,64 @@ const username = document.getElementById('username');
 const mes = document.getElementById('message');
 const loginButton = document.getElementById('login');
 let newWebSocket = null;
+const supportCommandList = ["addMessageToDocument","addUser","deleteUser"];
 
 // const request = new XMLHttpRequest();
-function addMessageToDocument(messageSender, messageContent) {
-    thisVue.$data.sites.push({text: messageSender + ':' + messageContent});
+function addMessageToDocument(sendTime, messageSender, messageContent) {
+    thisVue.$data.sites.push({text: sendTime + ' ' + messageSender + ':' + messageContent});
+}
+
+function addUser(inUsername, inLastLogin) {
+    userVue.$data.clientList.push({'username': inUsername, 'lastLogin': inLastLogin});
+}
+
+function updateUserList(userList) {//userList是列表，其中包含形如{}的键值对
+    userVue.$data.clientList = userList
+}
+
+function deleteUser(inUsername) {
+    for (let i = userVue.$data.clientList.length - 1; i >= 0; i--) {
+        if (userVue.$data.clientList[i].username === inUsername) userVue.$data.clientList.splice(i, 1);
+    }
 }
 
 //服务器与客户端的模拟握手的原理：客户端首先向服务器发起连接请求，连接打开后，客户端立即响应发送用户名,服务器处理用户名，连接正式建立。
 //客户端发送消息，仅仅是发送消息，并不做任何动作。
 //服务器接受到一条客户端发送的消息后，将这条消息存储，然后尝试广播给包含发送者在内的所有用户。
+//如果服务器发来的数据并不是JSON，就说明这是一条指令，指令是一个普通的字符串集，除首个元素，每个字符串都是encodeURIComponent编码的，由不编码的逗号","分隔。
+//第一个元素是函数名，也就是命令名，其余项解码后都是函数传入的参数。
 loginButton.onclick = function() {
     newWebSocket = new WebSocket('ws://localhost:8081');//发起连接请求
     newWebSocket.onopen = function(ev) {//连接打开且有效，客户端准备发送用户名
         newWebSocket.send(encodeURI(username.value));//客户端发送用户名
         newWebSocket.onmessage = function(messageEvent2) {//信息监听
-            let s = JSON.parse(decodeURI(messageEvent2.data));//信息内容（JSON串）
-            addMessageToDocument(s.poster, s.messageContent);//将信息打印在屏幕上
+            if (messageEvent2.data[0] !== '{') {//首个字符不是{，说明传入了命令。客户端只需要解码命令再执行就可以。
+                let s = decodeURI(messageEvent2.data);/*传入的命令是函数。s经过解码，会得到一个列表，1.18 23:49有问题*/
+                if (s[0] in supportCommandList) {//看看传入的函数在不在支持指令列表中，如果在，就执行函数，向函数传递参数.
+                }
+            } else {//常规消息字符串
+                let s = JSON.parse(messageEvent2.data);//信息内容（JSON串）
+                addMessageToDocument(s.time, decodeURI(s.poster), decodeURI(s.messageContent));//将信息打印在屏幕上
+            }
         };
     };
     loginButton.hidden = Boolean(1);//隐藏登录按钮，连接正式建立。
+    username.hidden = Boolean(1);
+    mes.hidden = Boolean(0);
+    submitButton.hidden = Boolean(0);
 };
 
-// const messageList = {
-//     numberOfMessage: 0,
-//     poster: [],
-//     messageContent: [],
-//     addPost: function (posterName, postMessage) {
-//         this.poster.push(posterName);
-//         this.messageContent.push(postMessage);
-//         this.numberOfMessage++
-//     },
-//     returnPostList: function (callback) {
-//         let cachePosterList = [];
-//         for (let i = 0; i <= this.numberOfMessage - 1; i++) {
-//             cachePosterList.push(callback(this.poster[i],this.messageContent[i]))
-//         }
-//         return cachePosterList
-//     }
-// };
+Vue.component('userList', {
+    props: ['user'],
+    template: '<p>{{ user.username }}</p><p>last login:{{ user.lastLogin }}</p>'
+});
 
+let userVue = new Vue({
+    el: '#onlinePeople',
+    data: {
+        clientList: []
+    }
+});
 Vue.component('todo-item', {
     props: ['todo'],
     template: '<dd>{{ todo.text }}</dd>'
@@ -55,16 +74,25 @@ let thisVue = new Vue({
         sites: []
     }
 });
-submitButton.onclick = function() {
-    //send the message to server
+// let clientVue = new Vue({
+//     el: '#onlinePeople'
+//     data:
+// });
+
+let submit = function() {
+    let time = new Date();
     let usernameWaitForSend = encodeURI(username.value);
     let messageContentWaitForSend = encodeURI(mes.value);
-    let messageWaitForSend = {poster: usernameWaitForSend, messageContent: messageContentWaitForSend};
+    let messageWaitForSend = {
+        poster: usernameWaitForSend,
+        messageContent: messageContentWaitForSend,
+        time: time.toTimeString().slice(0, 8)
+    };
     newWebSocket.send(JSON.stringify(messageWaitForSend));//仅仅是发送信息而已，并不把信息增加到页面上，等待服务端回传信息时添加。
-    // let url = "send";
-    // request.open("POST", url, true);
-    // request.setRequestHeader("authorization", encodeURI(username.value));
-    // request.send(mes.value);
-    // addMessageToDocument(username.value, mes.value);
+    mes.value = '';
 };
 
+submitButton.onclick = submit;//提交按钮点击事件
+mes.onkeydown = function(event) {
+    if (event.code === 'Enter' || event.code === 'NumpadEnter') submit();//enter按下事件
+};
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const existFile = ['/chat.html', '/404.html', '/main.js', '/server.js', '/favicon.ico', '/mainsolve.js', '/static/vue.min.js'];// First is main page,else are file exist on the server.
+const existFile = ['/chat.html', '/404.html', '/main.js', '/server.js', '/favicon.ico', '/mainsolve.js', '/static/vue.min.js', '/maincss.css'];// First is main page,else are file exist on the server.
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
@@ -23,8 +23,7 @@ const httpServer = http.createServer(function(request, response) {
                 response.end();
             }
         });
-    }
-    else {
+    } else {
         let realPath = path.join(__dirname, existFile[1]);
         fs.readFile(realPath, function(err, data) {
             if (err) {
@@ -44,7 +43,13 @@ console.log('http://localhost:8080/');
 //     'host': 'localhost'
 //
 // });
-let clientList = {};//username:socket
+let clientList = {};//在服务器端也要维护一个列表，列表格式如下：{username:{socket:,lastLogin:}}
+function broadcast(message){//服务器对所有在“在线列表”中的用户发送消息。
+    for (let key in clientList) {//客户端发送消息，服务器对其进行广播
+        if (clientList.hasOwnProperty(key)) clientList[key][socket].send(message);
+    }
+}
+
 //websocket service
 webSocketServer = new ws({
     server: httpServer,
@@ -53,8 +58,9 @@ webSocketServer = new ws({
 webSocketServer.on('connection', function(clientWebSocket) {//服务器建立监听，等待客户端连接
     clientWebSocket.on('close', function(code, reason) {
         for (let user in clientList) {
-            if (clientList[user] === clientWebSocket) {
+            if (clientList[user].socket === clientWebSocket) {
                 delete clientList[user];
+                broadcast(encodeURI("deleteUser("+user+")"))//删除用户，装船打包一波带走
                 // console.log("someone disconnect")
             }
         }
@@ -63,14 +69,13 @@ webSocketServer.on('connection', function(clientWebSocket) {//服务器建立监
         if (encodeMessage.toString()[0] === '{' && encodeMessage.toString()[encodeMessage.toString().length - 1] === '}') {
             // let messageContent = JSON.parse(encodeMessage.toString());
             //向数据库记录这条消息
-            for (let key in clientList) {//客户端发送用户名
-                if (clientList.hasOwnProperty(key)) clientList[key].send(encodeMessage);
-            }
-        } else {
+            broadcast(encodeMessage);//向所有用户发送这条消息
+        } else {//客户端发送用户名
             let username = decodeURI(encodeMessage.toString());
-            clientList[username] = clientWebSocket; //绑定用户名和socket
+            let dat = new Date();
+            clientList[username] = {"socket":clientWebSocket,"lastLogin":dat.toTimeString().slice(0,8)}; //绑定用户名和socket
+            broadcast("updateUserList")
             //向客户发送陈年老消息
         }
-
     });
 });
